288b94302c84a49e7f80608f82a24fb54db4ee5b
lolways <67913723+lolways@users.noreply.github.com>
2021-05-17 10:06:51 -0300

    guardado de contraseña

--- ps.py
+++ ps.py
@@ -3,12 +3,30 @@ import random
 chars = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ1234567890!#$%&/()=?¡¿,."
 
 while 1:
-    contraseña_lon= int(input("¿Que largo necesitas que la contraseña sea?"))
-    contraseña_cantd= int(input("¿cuantas contraseñas necesitas?"))
+    contraseña_lon= int(input("¿Que largo necesitas que la contraseña sea?: "))
+    
+    contraseña_cantd= int(input("¿cuantas contraseñas necesitas?: "))
+    
     for x in range(0, contraseña_cantd):
         print(x)
         contraseña = ""
+       
         for  x in range(0, contraseña_lon):
             contraseña_char = random.choice(chars)
             contraseña      = contraseña + contraseña_char
-        print("Aca esta su contraseña")
+       
+        print("Aca esta su contraseña: ", contraseña)
+        
+        archivo = open("contraseñas.txt", "w")
+
+        nc = input("para que es su contraseña: ")
+
+        archivo.write(nc + ":" + contraseña)
+
+        archivo.close()
+    exit()
+
+
+
+
+
